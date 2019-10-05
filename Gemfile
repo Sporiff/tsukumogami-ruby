33006fa04265a8068b5dc032544ff8ae8aa19318
Ciarán Ainsworth <ciaranainsworth@posteo.net>
2019-10-05 22:38:03 +0100

    Write web scraper for daily Yokai

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source "https://rubygems.org"
+
+gem 'elephrame'
+gem 'nokogiri'
+gem 'httpclient'
---
+++ main.rb
@@ -0,0 +1,47 @@
+require 'elephrame'
+require 'open-uri'
+require 'nokogiri'
+require 'httpclient'
+
+# Define the schedule, set at 12pm every day
+
+dailyYokai = Elephrame::Bots::Periodic.new '0 12 * * *'
+
+# Follow the random URL redirection
+
+random = 'http://yokai.com/?redirect_to=random'
+
+# Gather the final URL
+
+httpc = HTTPClient.new
+resp = httpc.get(random)
+puts resp.header['Location']
+
+# Strip the unnecessary characters
+
+link = resp.header['Location'].to_s.tr('[]""','')
+
+# Pull the HTML from the final page
+
+html = Nokogiri.HTML(open(link))
+
+# Take the page title and strip the trailing substring to return only the name of the creature
+
+yokainame = Nokogiri::HTML.parse(open(link)).title.to_s.gsub(" | Yokai.com", "")
+
+# Download the image from the page. Since this is always the first image, use html.at
+
+imagesource =  html.at('img')['src']
+File.open("yokai.png", "wb") do |f|
+  f.write(open(imagesource).read)
+end
+
+# Define the status to be posted
+
+status = "The Yōkai of the day is " + yokainame + ". #yokai #yokaioftheday\n \nArt and information by Matthew Meyer.\n" + "\n" + link
+
+# Post the status and the media together
+
+dailyYokai.run do |bot|
+  bot.post(status, media: ['./yokai.png'])
+end
